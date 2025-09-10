0e1dc6ef14356e86bc2bca43dd65771ba62da971
DevAyaan05 <ayaansyed2001@gmail.com>
2025-09-10 17:38:17 +0530

    " important Imports"

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -129,3 +129,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
